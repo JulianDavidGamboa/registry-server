3962a27feef38f8a3a2dd20b5df9c942e531576a
Julian David Gamboa <juliandg34@gmail.com>
2024-09-26 09:45:06 -0500

    Add registry server

---
+++ src/test/java/com/curso/app/registryserver/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.app.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
